0f8580c5415103d1da4792250e0ba7035be7c952
Lucas Ferreira <panchorf@gmail.com>
2016-04-06 17:57:37 -0300

    Get things working ;)

--- index.android.js
+++ index.android.js
@@ -13,42 +13,31 @@ var {
 } = React;
 
 var MapTest = React.createClass({
-
-  renderMap1:function(){
-    return <Map1/>;
-  },
-
-  renderMap2:function(){
-    return <Map2/>;
-  },
-
   renderScene:function(route,navigator){
-    var Scene = null;
     switch(route.id){
       case 'Map1':
-        Scene = this.renderMap1();
+        return (
+          <View style={{flex: 1}}>
+            <Map1 style={{flex: 1}} />
+          </View>
+        );
         break;
       default:
-        Scene = this.noRoute(navigator);
+        return this.noRoute(navigator);
         break;
     }
-    return Scene;
   },
-
-  render:function() {
-
-    //Return the following instead to ignore using the navigator 
-    //return <Map1/>;
-
+  render: function() {
+    // return this.renderMap1();
     return <Navigator
+            style={{flex: 1}}
             initialRoute={{id:'Map1'}}
             renderScene={this.renderScene}/>;
   },
-
   noRoute: function(navigator) {
     return <View><Text>{'No route here'}</Text></View>;
   }
-
 });
+
 
 AppRegistry.registerComponent('MapTest', () => MapTest);
